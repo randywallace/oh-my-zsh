d80fdf843404a91cf5710f3614904db64348e94b
Randy D. Wallace Jr <randy@randywallace.com>
2013-12-13 17:05:14 -0500

    Add a script that adds an aws-cli plugin for autocompletion

--- plugins/aws/aws.plugin.zsh
+++ plugins/aws/aws.plugin.zsh
@@ -1,17 +1,19 @@
-export AWS_HOME=~/.aws
+export AWS_CONFIG_FILE='/etc/aws_config.conf'
 
 function agp {
   echo $AWS_DEFAULT_PROFILE
-  
+
 }
 function asp {
   export AWS_DEFAULT_PROFILE=$1
-    export RPROMPT="<aws:$AWS_DEFAULT_PROFILE>"
-    
-}
-function aws_profiles {
-  reply=($(grep profile $AWS_HOME/config|sed -e 's/.*profile \([a-zA-Z0-9_-]*\).*/\1/'))
+  export RPROMPT="<aws:$AWS_DEFAULT_PROFILE>"
 }
 
-compctl -K aws_profiles asp
-source `which aws_zsh_completer.sh`
+function aws_profiles {
+  reply=($(grep profile $AWS_CONFIG_FILE | sed -e 's/.*profile \([a-zA-Z0-9_-]*\).*/\1/' ))
+}
+
+if which aws > /dev/null 2>&1; then
+  compctl -K aws_profiles asp
+  source $(which aws_zsh_completer.sh)
+fi
